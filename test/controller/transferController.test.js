70199e6d54a4a21dbbc855f88ee68a7629227659
isaha7x <isabellahcarolyna@hotmail.com>
2025-08-09 16:08:13 -0300

    adicionei o primeiro teste para validar o controller

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,27 @@
+// Bibliotecas
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+// Aplicação
+const app = require('../../app');
+
+// Testes
+describe('Transfer Controller', () => {
+    describe('POST /transfers', () => {
+        it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
+            const resposta = await request(app)
+                .post('/transfers')
+                .send({
+                    from: "Isa",
+                    to: "Guilherme",
+                    value: 100
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+        });
+    });
+
+    describe('GET /transfers', () => {
+        // Its ficam aqui
+    });
+});
